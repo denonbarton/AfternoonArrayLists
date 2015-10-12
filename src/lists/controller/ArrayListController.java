f43f40a83953c7fdefadafa8ea1aa3fa75fb763d
denonbarton <dbar0540@740s-pl-126.local>
2015-10-12 13:30:29 -0600

    1

--- src/lists/controller/ArrayListController.java
+++ src/lists/controller/ArrayListController.java
@@ -18,19 +18,34 @@ public class ArrayListController
 	{
 		firstWords.add("asdihfalskjdfhlaskjdfhakljsdfh");
 		firstWords.add("babababbababababababababa");
+		firstWords.add(1,"CRYING INSANELY");
 		/**
 		 *  displays the first word and then keeps moving on to the next
 		 */
 		displayList();
+		firstWords.set(2, "mamamamamamamamamamamamamamamamama");
+		displayList();
+		firstWords.remove(0);
+		displayList();
+		showOtherLoop();
 	}
 	
 	private void displayList()
 	{
+		popupDisplay.showList("The size of the list it " + firstWords.size());
 		for(int spot = 0; spot < firstWords.size(); spot++)
 		{
 			popupDisplay.showList("The contents at " + spot  + " are " + firstWords.get(spot));
-			
+			firstWords.remove(0);
 		}
 			
+	}
+	
+	private void showOtherLoop()
+	{
+		for(String words : firstWords)
+		{
+			popupDisplay.showList(words);
+		}
 	}
 }
